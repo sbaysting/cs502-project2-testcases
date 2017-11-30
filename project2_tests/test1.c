971c55e0ec420a22a0406a895e87912584b6a689
Priyank Jeevanlal Jain <jain206@data.cs.purdue.edu>
2017-11-29 21:46:39 -0500

    Added project 2 files

---
+++ project2_tests/test1.c
@@ -0,0 +1,8 @@
+int a, b;
+void init() {a =1;}
+int foo(int i){
+int c;
+init();
+if (i) {a = 0; b = 0;}
+printf("%d", a+b+c);
+}
---
+++ project2_tests/test3.c
@@ -0,0 +1,7 @@
+void init(int *q, int *p) { if (*p<0) *q=1;}
+int foo(int a){
+int i=0, b;
+init(&i, &b);
+b=0;
+printf("%d", a+b+i);
+}
